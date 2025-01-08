659c90a0f9ce7b7a8b1007845fc24718cb096b26
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2025-01-08 20:10:46 +0600

    add sign-out button to dashboard for user session management

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,19 @@
+'use client'
+
 import React from 'react';
+import { signOut } from 'next-auth/react';
 
 const page = () => {
-    return (
-        <div>
-            dashboard
-        </div>
-    );
+  return (
+    <div>
+      user logged in :
+
+      <div>
+        <button onClick={()=> signOut()}>Sign out</button>
+      </div>
+      
+    </div>
+  );
 };
 
 export default page;
